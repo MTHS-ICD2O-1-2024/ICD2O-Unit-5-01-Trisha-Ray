48755374a675023f2d086d1a8f229cc1ec59f76e
default <trisha.ray@stu.ocsb.ca>
2025-04-16 17:29:18 +0000

    index and js added

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Trisha Ray
+// Created on: April 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function calculateCelcius() {
+  // input
+  const fahrenheit = parseFloat(document.getElementById('fahrenheit').value);
+
+  // process
+  const celcius = (fahrenheit - 32) * 5/9 
+
+  // output
+  document.getElementById('temperature').innerHTML = 'The temperature in clecius is: ' + celcius + '°'
+}
+
